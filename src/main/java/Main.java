de04c4e30fd3bcc5244686d2068cfdfaa4d36eeb
Artyom-IWT <untila_artem@mail.ru>
2021-02-20 23:52:05 +0300

    creating fields() and whole class structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,12 +27,24 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         parser.kotlinFile();
-        File project = new File("D:\\UltimateIDEA\\KotlinAsFirst2019\\src\\lesson12\\task1\\PhoneBook.kt");
+        /*File project = new File("D:\\UltimateIDEA\\KotlinAsFirst2019\\src\\lesson12\\task1\\PhoneBook.kt");
         charStream = CharStreams.fromFileName(project.getAbsolutePath());
         KtLexer lexer = new KtLexer(charStream);
         TokenStream tokens = new CommonTokenStream(lexer);
         KtParser parser = new KtParser(tokens);
-        System.out.println("Running 'run()' :\n\n\n");
+        DirectoryScanner scanner = new DirectoryScanner();
+        scanner.setIncludes(new String[]{"**\\*.kt"});
+        System.out.println("Directory? " + project.isDirectory());
+        System.out.println("Directory exists? " + project.exists());
+        scanner.setBasedir(project);
+        scanner.scan();
+        String[] ktFiles = scanner.getIncludedFiles(); // тут должен быть foreach
+        for (String ktFile : ktFiles) {
+            File file = new File(project, ktFile);
+            String path = file.getAbsolutePath();
+            }
+        */
+        System.out.println("Running 'run()'\n\n\n");
         run();
         System.out.println("\n\n\nRunning 'fields()'\n\n\n");
         fields();
@@ -42,6 +54,9 @@ public class Main {
         int counter = 0;
         int bracesCounter = 0;
         int startIndex = 0;
+        int classCounter = 0;
+        boolean ok = false;
+
         for (int i = 1; i < tokens.size(); i++) {
             String prevType = lexer.getVocabulary().getSymbolicName(tokens.get(i - 1).getType());
             String prevName = tokens.get(i - 1).getText();
@@ -53,36 +68,87 @@ public class Main {
                             "Kotlin class founded = " + prevName + " " + name
                     );
                     startIndex = i + 1;
-                    System.out.println("startIndex = " + startIndex);
-                    for (int j = startIndex; j < tokens.size(); i++) {
-                        String type1 = lexer.getVocabulary().getSymbolicName(tokens.get(i).getType());
-                        if (type1.equals("LCURL")){
-                            bracesCounter++;
-                            startIndex = j + 1;
-                            System.out.println(
-                                    "LCURL founded.\nbracesCounter = " + bracesCounter + "\n" + "startIndex = " + startIndex
-                            );
-                            break;
-                        }
-                    }
+                    System.out.println("startIndex = " + startIndex + "\n");
+                    break;
                 }
+            }
+        }
+
+        for (int i = startIndex; i < tokens.size(); i++) {
+            String type = lexer.getVocabulary().getSymbolicName(tokens.get(i).getType());
+            if (type.equals("LCURL")) {
+                bracesCounter++;
+                startIndex = i + 1;
+                System.out.println(
+                        "LCURL founded\nbracesCounter = " + bracesCounter + "\n"
+                                + "startIndex = " + startIndex + "\n"
+                );
                 break;
             }
         }
-        for (int i = startIndex; i < tokens.size(); i++) {
+
+
+        for (int i = startIndex + 1; i < tokens.size(); i++) {
+
             String prevType = lexer.getVocabulary().getSymbolicName(tokens.get(i - 1).getType());
             String prevName = tokens.get(i - 1).getText();
-            //if (prevType.equals("FUN"))
+
+            if (prevType.equals("FUN")) {
+                String name = tokens.get(i).getText();
+                System.out.println("Founded fun : " + prevName + " " + name + "\n");
+                startIndex = i + 2;
+
+                for (int j = startIndex; j < tokens.size(); j++) { // +2 из-за скобок
+                    String typeF = lexer.getVocabulary().getSymbolicName(tokens.get(j).getType());
+                    if (typeF.equals("ASSIGNMENT")) {
+                        startIndex = j + 1;
+                        break;
+                    }
+                    if (typeF.equals("LCURL")) {
+                        bracesCounter++;
+                        System.out.println(
+                                "LCURL founded\nbracesCounter = " + bracesCounter + "\n"
+                                        + "Index = " + j + "\n"
+                        );
+                        startIndex = j + 1;
+                        break;
+                    }
+                }
+
+                while (bracesCounter != 1) {
+                    String typeW = lexer.getVocabulary().getSymbolicName(tokens.get(startIndex).getType());
+                    if (typeW.equals("LCURL")) {
+                        bracesCounter++;
+                        System.out.println(
+                                "LCURL founded\nbracesCounter = " + bracesCounter + "\n"
+                                        + "Index = " + startIndex + "\n"
+                        );
+                    }
+                    if (typeW.equals("RCURL")) {
+                        bracesCounter--;
+                        System.out.println(
+                                "RCURL founded\nbracesCounter = " + bracesCounter + "\n"
+                                        + "Index = " + startIndex + "\n"
+                        );
+                    }
+                    startIndex++;
+                }
+
+                i = startIndex;
+
+            }
+
             if (prevType.equals("VAL") || prevType.equals("VAR")) {
                 String type = lexer.getVocabulary().getSymbolicName(tokens.get(i).getType());
                 String name = tokens.get(i).getText();
                 if (type.equals("Identifier")) {
                     counter++;
                     System.out.println(
-                            "Founded field : " + prevName + " " + name + "\n" + "Count = " + counter
+                            "Founded field : " + prevName + " " + name + "\n" + "Count = " + counter + "\n"
                     );
                 }
             }
+
         }
     }
 
@@ -98,9 +164,7 @@ public class Main {
     public static void skipFun() {
     }
 
-    public static void
-
-    public static void overrides() throws IOException {
+    public static void findClass() throws IOException {
         for (int i = 0; i < tokens.size() - 1; i++) {
             String tokenType = lexer.getVocabulary().getSymbolicName(tokens.get(i).getType());
             String tokenText = tokens.get(i).getText();
@@ -145,7 +209,7 @@ public class Main {
             }
         }
         System.out.println("Count = " + counter);*/
-        for (int i = 0; i < tokens.size() ; i++) {
+        for (int i = 0; i < tokens.size(); i++) {
             String tokenText = tokens.get(i).getText();
             String lexerRule = lexer.getVocabulary().getSymbolicName(tokens.get(i).getType());
             if (!lexerRule.equals("NL")) //skipping NewLine tokens (for beauty)
