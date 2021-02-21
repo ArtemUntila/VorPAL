88edc907ec76d9f98df935ede27b1f59e11bdb42
Artyom-IWT <untila_artem@mail.ru>
2021-02-21 12:08:41 +0300

    fields, metrics, overrides

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,56 +1,142 @@
 import org.antlr.v4.runtime.*;
 import org.apache.tools.ant.DirectoryScanner;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.Set;
 
 public class Main {
 
-    private static CharStream charStream;
-
-    static {
-        try {
-            charStream = CharStreams.fromFileName(
-                    "D:\\UltimateIDEA\\KotlinAsFirst2019\\src\\lesson12\\task1\\PhoneBook.kt");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static final KtLexer lexer = new KtLexer(charStream);
-    private static final TokenStream tokens = new CommonTokenStream(lexer);
-    private static final KtParser parser = new KtParser(tokens);
-
     public static void main(String[] args) throws IOException {
-        parser.kotlinFile();
-        /*File project = new File("D:\\UltimateIDEA\\KotlinAsFirst2019\\src\\lesson12\\task1\\PhoneBook.kt");
-        charStream = CharStreams.fromFileName(project.getAbsolutePath());
-        KtLexer lexer = new KtLexer(charStream);
-        TokenStream tokens = new CommonTokenStream(lexer);
-        KtParser parser = new KtParser(tokens);
+
+        File project = new File("D:\\UltimateIDEA\\kotlin-project\\src\\main\\kotlin");
         DirectoryScanner scanner = new DirectoryScanner();
         scanner.setIncludes(new String[]{"**\\*.kt"});
+
         System.out.println("Directory? " + project.isDirectory());
         System.out.println("Directory exists? " + project.exists());
+
         scanner.setBasedir(project);
         scanner.scan();
+
         String[] ktFiles = scanner.getIncludedFiles(); // тут должен быть foreach
+
         for (String ktFile : ktFiles) {
             File file = new File(project, ktFile);
             String path = file.getAbsolutePath();
+            CharStream charStream = CharStreams.fromFileName(path);
+            KtLexer lexer = new KtLexer(charStream);
+            TokenStream tokens = new CommonTokenStream(lexer);
+            KtParser parser = new KtParser(tokens);
+            parser.kotlinFile();
+
+            /*for (int i = 0; i < tokens.size(); i++) {
+                String tokenText = tokens.get(i).getText();
+                String lexerRule = lexer.getVocabulary().getSymbolicName(tokens.get(i).getType());
+                if (!lexerRule.equals("NL")) //skipping NewLine tokens (for beauty)
+                    System.out.println(tokenText + "\t\t" + lexerRule);
+
+                //Simple way
+                //System.out.println(tokens.get(i).toString());
+            }*/
+
+            parse(tokens, lexer);
             }
-        */
         System.out.println("Running 'run()'\n\n\n");
-        run();
+        //run();
         System.out.println("\n\n\nRunning 'fields()'\n\n\n");
-        fields();
+        //fields();
     }
 
-    public static void fields() {
+    public static void parse(TokenStream tokens, KtLexer lexer) {
+
+        int counter = 0;
+        int bracesCounter = 0;
+        //int startIndex = 0;
+        int classCounter = 0;
+        boolean classBody = false;
+        boolean otherBody = false;
+        int funCounter = 0;
+
+        Set<String> metricA = Set.of(
+                "ASSIGNMENT", "ADD_ASSIGNMENT", "SUB_ASSIGNMENT", "MULT_ASSIGNMENT", "DIV_ASSIGNMENT", "MOD_ASSIGNMENT",
+                 "INCR", "DECR"
+        );
+
+        int counterA = 0;
+
+        int counterB = 0;
+
+        Set<String> metricC = Set.of(
+                "LANGLE", "RANGLE", "LE", "GE", "EXCL_EQ", "EQEQ", "EXCL_EQEQ", "EQEQEQ", "QUEST", "ELSE", "TRY",
+                "CATCH", "WHEN", "IF"
+                );
+
+        int counterC = 0;
+
+        int overrides = 0;
+
+        int classBraces = 0;
+
+        for (int i = 1; i < tokens.size(); i++) {
+
+            String prevType = lexer.getVocabulary().getSymbolicName(tokens.get(i - 1).getType());
+            String prevName = tokens.get(i - 1).getText();
+            String type = lexer.getVocabulary().getSymbolicName(tokens.get(i).getType());
+            String name = tokens.get(i).getText();
+
+            if (prevType.equals("CLASS") && type.equals("Identifier")) {
+                classCounter++;
+                classBraces++;
+                System.out.println("classBraces = " + classBraces);
+            }
+
+            // исключаем переменные, объявленные в методах или функциях вне класса
+            if (prevType.equals("FUN") && type.equals("Identifier")) {
+                classBody = false;
+                System.out.println("Fun : " + name);
+            }
+
+            if (prevType.equals("LCURL"))
+                bracesCounter++;
+            if (prevType.equals("RCURL")) {
+                bracesCounter--;
+                if (bracesCounter < classBraces) {
+                    classBraces = bracesCounter;
+                    System.out.println("classBraces changed and = " + classBraces);
+                }
+            }
+
+            //подсчёт метрик
+            if (metricA.contains(type))
+                counterA++;
+            if (metricC.contains(type))
+                counterC++;
+            if(type.equals("Identifier") && !prevType.equals("FUN")) {
+                String nextType = lexer.getVocabulary().getSymbolicName(tokens.get(i + 1).getType());
+                if (nextType.equals("LPAREN")) counterB++;
+            }
+
+            if (prevType.equals("OVERRIDE") && type.equals("FUN"))
+                overrides++;
+
+            classBody = classBraces > 0 && bracesCounter == classBraces;
+            if (classBody && ((prevType.equals("VAL") || prevType.equals("VAR")) && type.equals("Identifier"))) {
+                counter++;
+                System.out.println("Field: " + prevType + name);
+            }
+        }
+
+        System.out.println("counter = " + counter);
+        System.out.println("classCounter = " + classCounter);
+        System.out.println("A = " + counterA + " ; B = " + counterB + " ; C = " + counterC);
+        System.out.println("Overrides = " + overrides);
+    }
+
+
+
+    public static void fields(TokenStream tokens, KtLexer lexer) {
         int counter = 0;
         int bracesCounter = 0;
         int startIndex = 0;
@@ -161,24 +247,9 @@ public class Main {
             return true;
         } else return false;
     }*/
-    public static void skipFun() {
-    }
 
-    public static void findClass() throws IOException {
-        for (int i = 0; i < tokens.size() - 1; i++) {
-            String tokenType = lexer.getVocabulary().getSymbolicName(tokens.get(i).getType());
-            String tokenText = tokens.get(i).getText();
-            String nextTokenType = lexer.getVocabulary().getSymbolicName(tokens.get(i + 1).getType());
-            if (tokenType.equals("CLASS") && nextTokenType.equals("Identifier")) {
-                System.out.println(
-                        "Kotlin class founded. Proof:\ntokenType = " + tokenType + "\ntokenText = " + tokenText
-                );
-            }
-        }
-    }
-
-    public static void run() throws IOException {
-        /*DirectoryScanner scanner = new DirectoryScanner();
+    /*public static void run() throws IOException {
+        DirectoryScanner scanner = new DirectoryScanner();
         scanner.setIncludes(new String[]{"**\\*.kt"});
         System.out.println("Directory exists = " + project.exists());
         System.out.println("Directory? " + project.isDirectory());
@@ -208,7 +279,7 @@ public class Main {
                 line = reader.readLine();
             }
         }
-        System.out.println("Count = " + counter);*/
+        System.out.println("Count = " + counter);
         for (int i = 0; i < tokens.size(); i++) {
             String tokenText = tokens.get(i).getText();
             String lexerRule = lexer.getVocabulary().getSymbolicName(tokens.get(i).getType());
@@ -218,5 +289,5 @@ public class Main {
             //Simple way
             //System.out.println(tokens.get(i).toString());
         }
-    }
+    }*/
 }
